064ade095140308b2e80985d69f1a0415fb6c445
Karthik K M <karthikkm5555@gmail.com>
2026-06-26 14:16:48 +0530

    update

--- dump-config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-
-const config = defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-});
-console.log(JSON.stringify(config, null, 2));
